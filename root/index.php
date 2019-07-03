762ce455d03f05924de67f1631d438da06e64398
Bright Robert <guruskid@yahoo.com>
2019-07-03 17:29:31 +0100

    CBT

---
+++ root/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>My CBT</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+	
+</head>
+<body>
+	<div id="container">
+	  <div id="topcontent"></div>
+		<div id="content">
+		<h1>WELCOME TO MyCBT Web Apps</h1>
+			<p>HELLO.</p>
+			<p>WELCOME TO MY CBT WEB APPS. </p>
+			<p>
+				<a href="http://localhost:8080/cbt/index.php" target="_blank">Click here to go the main CBT app</a>
+			</p>
+			<p><a href="http://localhost:8080/cbt/cbtadmin/index.php" target="_blank">Click here to go the admin page</a></p>
+			<p>FOR THE ADMIN PAGE. USERNAME IS admin AND PASSWORD IS admin,	in	lowercase			</p>
+			<p>&nbsp;</p>
+			<p>Adeyinka Idowu Adeumi</p>
+			<p>08135567221</p>
+			<p>iadeyinka03@gmail.com</p>
+
+
+		</div>
+		<div id="botcontent"></div>
+		<div id="footer">Copyright PLG<a style="color: #81ACCE;" href="http://www.border-it.nl/"></a></div>
+	</div>
+</body>
+</html>
